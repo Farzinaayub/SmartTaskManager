b05ce776aa3144899b9bfda1f5e72ca3cba711fe
Farzeena P A <farzinaayub266@gmail.com>
2023-04-18 22:41:38 +0530

    review commit
    voice input is taking from console and dictionary is returned. task.task is rendered at dashboard.html. task.date,task.time etc..rendered at tab2.
    On admin page, task and category tables are thre. fine

--- base/models.py
+++ base/models.py
@@ -1,17 +1,42 @@
 from django.db import models
-
+from datetime import date
 # Create your models here.
 
 
+class Category(models.Model):
+    # task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+# class Date(models.Model):
+#     # task = models.ForeignKey(Task, on_delete=models.CASCADE)
+#     # date = models.DateField(default=date.today, null=True)
+
+#     def __str__(self):
+#         return str(self.date)
+
+
+# class Desc(models.Model):
+#     # task = models.ForeignKey(Task, on_delete=models.CASCADE)
+#     # desc = models.TextField(null=True, blank=True)
+
+#     def __str__(self):
+#         return str(self.desc[0:20])
+
+
 class Task(models.Model):
-    Id = models.CharField(max_length=200)
-    Action = models.CharField(max_length=200)
-    Task = models.CharField(max_length=200, null=True)
-    Time = models.CharField(max_length=200, null=True)
-    Relative_time = models.CharField(max_length=200, null=True)
-    stakeholders = models.CharField(max_length=200, null=True)
-    category = models.CharField(max_length=200)
-    priority = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    # desc = models.ForeignKey(
+    #     Desc, on_delete=models.SET_NULL, null=True)
+    desc = models.TextField(null=True, blank=True)
+    # date = models.ForeignKey(
+    #     Date, on_delete=models.SET_NULL, null=True)
+    date = models.DateField(default=date.today, null=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
