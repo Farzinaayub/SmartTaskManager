3c79e77f9186e10451759f040d9f105d2b3dde5b
Farzeena P A <farzinaayub266@gmail.com>
2023-06-06 10:14:13 +0530

    test

--- tasktodo/settings.py
+++ tasktodo/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'tasktodo.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {'default': dj_database_url.config(        # Feel free to alter this value to suit your needs.
-    default='postgresql://postgres:postgres@localhost:5432/mysite',
+    default='postgresql://postgres:postgres@localhost:5432/tasktodo',
     conn_max_age=600)}
 
 
@@ -137,7 +137,6 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 
 # Default primary key field type
